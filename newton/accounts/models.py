e9be7f162f29ba41b35d73a149f0d6190a795cb6
Luis Baquera <luis.baquera@ucr.edu>
2024-08-07 02:09:15 -0700

    Extend user manager to use phone as username

--- newton/accounts/models.py
+++ newton/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .manager import UserManager
 
 # Create your models here.
+class GFG(AbstractUser):
+    phone = models.CharField(max_length=12, unique=True)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
